47750d2938e1bffc4f81a2614fb2b6bc91effea5
vttoteva25 <viktoriya.toteva@abv.bg>
2024-03-12 22:45:13 +0200

    Add CustomersController

---
+++ BS.WebApiServices/Controllers/CustomersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BS.WebApiServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class CustomersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
